72389ff8585241ee6e41ebb890519f34f375b1a9
Eric Wilson <eric.wilson@certara.com>
2019-03-07 19:34:40 -0500

    added environment check for production

--- src/app/services/google/analytics/google-analytics.service.ts
+++ src/app/services/google/analytics/google-analytics.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { Router, NavigationEnd } from '@angular/router';
-
+import { environment } from '../../../../environments/environment';
 // --- use a declare to allow the compiler find the ga function
 declare let ga: Function;
 
@@ -30,9 +30,12 @@ export class GoogleAnalyticsService {
    */
   public trackEvent(category: string, label: string, action: string = null, value: number = null) {
     try {
-      ga('send', 'event', { eventCategory: category, eventLabel: label,
-        eventAction: action, eventValue: value
-      } );
+      if(environment.production)
+      {
+        ga('send', 'event', { eventCategory: category, eventLabel: label,
+          eventAction: action, eventValue: value
+        } );
+      }
     } catch (error) {
       console.log(`error: ${error}`);
     }
@@ -43,7 +46,7 @@ export class GoogleAnalyticsService {
 
 
   public subscribe() {
-    if (!this.subscription) {
+    if (!this.subscription && environment.production) {
       // subscribe to any router navigation and once it ends, write out the google script notices
       this.subscription = this.router.events.subscribe( e => {
         if (e instanceof NavigationEnd) {
